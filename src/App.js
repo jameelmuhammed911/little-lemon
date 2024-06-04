46234e5ea3dbc2909cd82cc1ae067f5d21aad581
Muhammed jameel Shaik <jameelmuhammed911@gmail.com>
2024-06-04 12:54:13 -0500

    Added Semantics to App.js and added files Header.js, Nav.js, Main.js and Footer.js to Components Folder

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
